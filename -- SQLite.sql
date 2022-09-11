a37becf4ac15ac4d945aecee570020558c5efeb8
Your Name <you@example.com>
2022-09-11 14:07:47 +0200

    price plan first commit

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -1,7 +1,2 @@
 -- SQLite
-select * from price_plan;
-
-SELECT id, plan_name, sms_price, call_price
-FROM price_plan where plan_name = 'call 101';
-
-update  price_plan set sms_price = ?, call_price = ? where plan_name = ?
+SELECT sms_price,call_price FROM price_plan WHERE plan_name = 'call 201';
